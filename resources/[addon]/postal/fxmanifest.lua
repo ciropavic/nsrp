35b95447357fbf1bf27913932f9dadd9edd14170
The_Noxus <35572113+the-noxus@users.noreply.github.com>
2022-12-07 17:24:30 -0800

    core link

---
+++ resources/[addon]/postal/fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Priest0802'
+description 'Origional Postal Codes Streaming from Server to Client'
+version '1.0.6'
+
+client_script 'client/client.lua'
+data_file('GTXD_PARENTING_DATA')('client/mapzoomdata.meta')
